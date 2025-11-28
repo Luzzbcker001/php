598fb341f8933fe28a0c6f40a8da278d2b3598f2
Luzzbcker001 <luzz.bcker@gmail.com>
2025-11-27 22:37:59 -0300

    Update entprocsaida.php

--- entprocsaida.php
+++ entprocsaida.php
@@ -1,20 +1,26 @@
 <?php
-echo "Digite um número: ";
-$numero = trim(fgets(STDIN));
+if (isset($_POST['numero'])) {
+    $numero = $_POST['numero'];
 
-if (!is_numeric($numero)) {
-    echo "Por favor, digite um valor numérico.\n";
-    exit;
-}
-
-// Processamento: compara com 10
-if ($numero > 10) {
-    echo "$numero é maior que 10.\n";
-} elseif ($numero < 10) {
-    echo "$numero é menor que 10.\n";
-} else {
-    echo "$numero é igual a 10.\n";
+    if (!is_numeric($numero)) {
+        echo "Por favor, digite um valor numérico.<br>";
+    } else {
+        if ($numero > 10) {
+            echo "$numero é maior que 10.<br>";
+        } elseif ($numero < 10) {
+            echo "$numero é menor que 10.<br>";
+        } else {
+            echo "$numero é igual a 10.<br>";
+        }
+    }
 }
 ?>
+
+<form method="POST">
+    Digite um número:
+    <input type="text" name="numero">
+    <button type="submit">Enviar</button>
+</form>
+
 <br>
 <a href="index.html">Voltar</a>
